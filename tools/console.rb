857e74fdd1bea05e6269ee73f3668f7268140ea1
github username <github email address>
2019-02-26 14:32:12 +0000

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,34 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+# Gym
+gym1 = Gym.new("Virgin")
+gym2 = Gym.new("Fitness First")
+gym3 = Gym.new("Nuffield Health")
+
+# Lifter
+maz = Lifter.new("Maz", 100)
+louise = Lifter.new("Louise", 56)
+aimee = Lifter.new("Aimee", 160)
+
+# Membership
+membership1 = Membership.new(maz, gym1, 36)
+membership2 = Membership.new(louise, gym2, 97)
+membership3 = Membership.new(aimee, gym3, 25)
+membership4 = Membership.new(maz, gym3, 82)
+membership5 = Membership.new(louise, gym3, 73)
+membership6 = Membership.new(aimee, gym1, 45)
+membership7 = Membership.new(maz, gym2, 76)
+
+#Lifter #instance methods
+# maz.get_memberships
+# aimee.get_gyms
+# Lifter.average_lift_total
+# louise.sign_up(gym3, 678)
+
+#Gym #instance methods
+# gym3.get_gym_memberships
+
 
 binding.pry
 
