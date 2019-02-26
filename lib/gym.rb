9322d73dfe0fae0e6d68043b0a7de0f96e2652ef
CalieR <calielrushton@gmail.com>
2019-02-26 10:53:19 +0000

    complete

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,47 @@
+# Get a list of all gyms
+# get a list of all memberships at a specific gym
+# Get a list of all the lifters that have a membership to a specific gym
+# Get a list of the names of all lifters that have a membership to that gym
+# Get the combined lift total of every lifter has a membership to that gym
+
+
 class Gym
-  attr_reader :name
+  attr_reader :name, :membership
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+# get a list of all memberships at a specific gym
+  def get_memberships
+    Membership.all.select { |membership| membership.gym == self }
+  end
+
+# Get a list of all the lifters that have a membership to a specific gym
+  def get_lifters
+    get_memberships.collect { |membership| membership.lifter }
+  end
+
+# Get a list of the names of all lifters that have a membership to that gym
+  def get_lifters_names
+    get_lifters.collect { |lifter| lifter.name }
+  end
+
+# Get the combined lift total of every lifter has a membership to that gym
+  def get_lifters_total_lifts
+    get_lifters.collect { |lifter| lifter.lift_total }.reduce(:+)
+  end
+  # First try gave:
+  # "NoMethodError: undefined method `+' for nil:NilClass"
+  # TODO: investigate....
+    # total_lifts = 0
+    # get_lifters.each { |lifter| total += lifter.lift_total }
+    # total_lifts
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,26 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+l1 = Lifter.new("Jeff", 10)
+l2 = Lifter.new("Bob", 50)
+l3 = Lifter.new("Helen", 80)
+
+g1 = Gym.new("Big Muscle Gym")
+g2 = Gym.new("Weedy Wonders")
+g3 = Gym.new("Raaaaah!")
+
+m1 = Membership.new(l1, g1, 100)
+m2 = Membership.new(l1, g2, 500)
+m3 = Membership.new(l1, g3, 800)
+m4 = Membership.new(l2, g1, 2500)
+m5 = Membership.new(l2, g2, 5000)
+m6 = Membership.new(l2, g3, 10000)
+m7 = Membership.new(l3, g1, 24)
+m8 = Membership.new(l3, g2, 61)
+m9 = Membership.new(l3, g3, 33)
+
+
+
 binding.pry
 
 puts "Gains!"
