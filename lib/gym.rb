36574fdb9d16241d4a19a2ed105da86e5236a08a
github username <github email address>
2019-02-26 09:10:33 +0000

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,24 @@
 class Gym
   attr_reader :name
+  @@gyms = []
 
   def initialize(name)
     @name = name
+  end
+
+  def self.all
+    @@gyms
+  end
+
+  def this_gym_memberships
+    Membership.all.select{|membership| membership.gym == self}
+  end
+
+  def member_names
+    this_gym_memberships.map{|membership| membership.lifter.name}
+  end
+
+  def lifter_totals_combined
+    this_gym_memberships.map{|membership| membership.lifter.lift_total}.inject(0, :+)
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,22 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+ali = Lifter.new("Ali", 370)
+ben = Lifter.new("Ben", 180)
+yas = Lifter.new("Yas", 250)
+
+pure = Gym.new("Pure")
+anytime = Gym.new("Anytime")
+easy = Gym.new("Easy Gym")
+
+ali_p = Membership.new(pure, ali, 25)
+ali_a = Membership.new(anytime, ali, 35)
+ben_e = Membership.new(easy, ben,)
+yas_a = Membership.new(anytime, yas, 35)
+yas_e = Membership.new(easy, yas)
+
+ali.sign_up(easy, 15)
+
 binding.pry
 
 puts "Gains!"
